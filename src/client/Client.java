852bc3346111b6be5583f1807a52cebf729f38f6
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-11-02 17:18:39 +0100

    client message coucou

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,47 @@
 package client;
 
-public class Client {
- // COUCOU TOI !! WINNERS
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import org.omg.CORBA.portable.UnknownException;
+
+public class Client implements Runnable{
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedInputStream in;
+	public Client(String host, int port) {
+		try {
+			clientSocket = new Socket(host, port);
+		} catch (UnknownException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		System.err.println("Lancement du traitement de connection cliente");
+		while (!clientSocket.isClosed()) {
+			try {
+				out = new PrintWriter(clientSocket.getOutputStream(), true);
+				in = new BufferedInputStream(clientSocket.getInputStream());
+				String commande = "Coucou JeansJacques";
+				out.write(commande);
+				out.flush();
+				System.out.println("Commande "+commande+" envoyée au serveur");
+				clientSocket.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+	}
+	public static void main(String[] args) {
+		Client cli = new Client("10.212.100.252", 4000);
+		cli.run();
+	}
 }
+
+
