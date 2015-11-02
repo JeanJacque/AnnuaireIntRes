9d0eabc7697833eef54431a0fdee8139af1dbed8
JeanJacque <theo.donzelle@gmail.com>
2015-11-02 16:54:11 +0100

    ajout version simple serveur

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -3,10 +3,28 @@
  */
 package serveur;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
+
 /**
  * @author Théo Donzelle
  *
  */
 public class Serveur {
-
+    
+    public void run(){
+        try {
+            ServerSocket socketServer = new ServerSocket(4000);
+            Socket socketduserveur = socketServer.accept();
+            System.out.println("Connexion etablie");
+        }
+        catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+    
 }
