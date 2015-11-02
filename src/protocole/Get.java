b087338cbdd081075100603c72bde971ca1b30dd
Alexandre CAZALA <alexandre.cazala@gmail.com>
2015-11-02 16:16:20 +0100

    get all

--- src/protocole/Get.java
+++ src/protocole/Get.java
@@ -1,5 +1,19 @@
 package protocole;
 
-public class Get {
+import java.util.HashMap;
 
+public class Get {
+	private HashMap<String,String> datas;
+	
+	public Get() {
+		
+	}
+	
+	public void setDatas(HashMap<String, String> result) {
+		datas = result;
+	}
+	
+	public HashMap<String, String> getDatas() {
+		return datas;
+	}
 }
