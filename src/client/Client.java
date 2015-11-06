171c6a02c543b385aa536de728d3bc3fb94e5604
JeanJacque <theo.donzelle@gmail.com>
2015-11-06 15:03:06 +0100

    javadoc classes client & serveur

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,37 +7,61 @@ import java.net.Socket;
 
 import org.omg.CORBA.portable.UnknownException;
 
-public class Client implements Runnable{
-	private Socket clientSocket;
-	private PrintWriter out;
-	private BufferedInputStream in;
-	public Client(String host, int port) {
-		try {
-			clientSocket = new Socket(host, port);
-		} catch (UnknownException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+/**
+ * 
+ * @author Alexandre Cazala, Théo Donzelle
+ * 
+ *         Classe client, elle permet d'établir une connexion avec le serveur,
+ *         d'échanger des données avec celui-ci et ainsi d'utiliser les services
+ *         proposés par le serveur.
+ */
+public class Client implements Runnable {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedInputStream in;
 
-	public void run() {
-		System.err.println("Lancement du traitement de connection cliente");
-		while (!clientSocket.isClosed()) {
-			try {
-				out = new PrintWriter(clientSocket.getOutputStream(), true);
-				in = new BufferedInputStream(clientSocket.getInputStream());
-				String commande = "Coucou JeansJacques";
-				out.write(commande);
-				out.flush();
-				System.out.println("Commande "+commande+" envoy�e au serveur");
-				clientSocket.close();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		}
+    /**
+     * .
+     * 
+     * @param host
+     *            String contenant l'adresse IP du serveur auquel le client va
+     *            essayer de se connecter
+     * @param port
+     *            Int contenant le port sur lequelle le client se connectera
+     * 
+     *            Constructeur du client qui etablie la connexion avec le
+     *            serveur
+     */
+    public Client(String host, int port) {
+        try {
+            clientSocket = new Socket(host, port);
+        } catch (UnknownException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	}
+    /**
+     * Methode permettant d'echanger des requetes avec le serveur et de traiter
+     * les réponses, tant que la connexion n'est pas fermée
+     */
+    public void run() {
+        System.err.println("Lancement du traitement de connection cliente");
+        while (!clientSocket.isClosed()) {
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedInputStream(clientSocket.getInputStream());
+                String commande = "Coucou JeansJacques";
+                out.write(commande);
+                out.flush();
+                System.out.println("Commande " + commande
+                        + " envoy�e au serveur");
+                clientSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+    }
 }
-
-
--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package serveur;
 
 import java.io.BufferedReader;
@@ -13,7 +10,12 @@ import java.net.Socket;
 import client.Client;
 
 /**
- * @author Théo Donzelle
+ * 
+ * Classe serveur, elle permet d'établir avec un client, d'échanger des données
+ * avec celui-ci et ainsi de permettre au client connecté d'utiliser les
+ * services proposés.
+ * 
+ * @author Alexandre Cazala, Théo Donzelle
  * 
  */
 public class Serveur {
@@ -22,6 +24,15 @@ public class Serveur {
     private BufferedReader in;
     private PrintWriter out;
 
+    /**
+     * 
+     * Constructeur du serveur qui etablie la connexion avec un serveur
+     * 
+     * @param port
+     *            Int contenant le port sur lequelle le client pourra se
+     *            connecter
+     * 
+     */
     public Serveur(int port) {
         try {
             socketServer = new ServerSocket(port);
@@ -31,7 +42,11 @@ public class Serveur {
             System.err.println(e);
         }
     }
-    
+
+    /**
+     * Methode permettant d'echanger des requetes avec le client et de les
+     * traiter, tant que la connexion n'est pas fermée
+     */
     public void run() {
         try {
             in = new BufferedReader(new InputStreamReader(
