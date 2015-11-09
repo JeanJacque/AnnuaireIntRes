637bc7a93b54f5e0c3335f34e767557589d1c5e4
JeanJacque <theo.donzelle@gmail.com>
2015-11-09 15:49:57 +0100

    Zboob

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -2,10 +2,15 @@ package serveur;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+
+import protocole.Request;
 
 import client.Client;
 
@@ -23,6 +28,7 @@ public class Serveur {
     private Socket socketduserveur;
     private BufferedReader in;
     private PrintWriter out;
+    private HashMap<String, String> datas;
 
     /**
      * 
@@ -41,6 +47,7 @@ public class Serveur {
         } catch (IOException e) {
             System.err.println(e);
         }
+        datas = new HashMap<String, String>();
     }
 
     /**
@@ -49,15 +56,18 @@ public class Serveur {
      */
     public void run() {
         try {
-            in = new BufferedReader(new InputStreamReader(
-                    socketduserveur.getInputStream()));
-            String message_distant = in.readLine();
-
-            System.out.println(message_distant);
+            InputStream is = socketduserveur.getInputStream(); 
+            ObjectInputStream ois = new ObjectInputStream(is);
+            
+            Request r = (Request) ois.readObject();
+            r.exec(datas);
             socketduserveur.close();
             socketServer.close();
         } catch (IOException e) {
             System.err.println(e);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
