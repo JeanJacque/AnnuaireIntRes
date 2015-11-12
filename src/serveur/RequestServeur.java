513cb3a424851011392b2928faace59e3e4c0ca7
JeanJacque <theo.donzelle@gmail.com>
2015-11-12 17:07:19 +0100

    requuest serveur

---
+++ src/serveur/RequestServeur.java
@@ -0,0 +1,73 @@
+package serveur;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+import protocole.Request;
+
+/**
+ * @author Théo Donzelle
+ * 
+ */
+public class RequestServeur implements Runnable {
+
+    private Socket socket;
+    private Hashtable<String, String> datas;
+    private InputStream is;
+    private ObjectInputStream ois;
+    private OutputStream os;
+    private ObjectOutputStream oos;
+
+    public RequestServeur(Socket s, Hashtable<String, String> d) {
+        this.socket = s;
+        this.datas = d;
+        try {
+            // Flux d'entree
+            is = socket.getInputStream();
+            ois = new ObjectInputStream(is);
+            // Flux de sortie
+            os = socket.getOutputStream();
+            oos = new ObjectOutputStream(os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        int nb = 0;
+        try {
+            Request r;
+            
+            // Boucle d'envoie et de reception de requetes
+            while (ois != null) {
+                r = (Request) ois.readObject();
+                r.exec(datas);
+                oos.writeObject(r);
+                nb++;
+            }
+        } catch (IOException e) {
+            System.err
+                    .println("Connexion interrompue par le client. (Requetes executees : "
+                            + nb + ")");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            // Fermeture de la socket
+            try {
+                socket.close();
+                System.out.println("Socket fermee.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
